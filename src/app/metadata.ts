ee18adfce1fcebd111171f9f0a50b68b4d0dd419
caffein.junkie <dev.satrio.ap@gmail.com>
2026-06-02 01:11:28 +0700

    feat: add metadata

---
+++ src/app/metadata.ts
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+
+export const siteMetadata: Metadata = {
+  title: {
+    default: 'FitGirl Repack FuckingFast Downloader',
+    template: '%s | FitGirl Repack FuckingFast Downloader',
+  },
+  description: 'FitGirl Repack FuckingFast Downloader - Download games from FitGirl Repack with ease.',
+  applicationName: 'FitGirl Repack FuckingFast Downloader',
+  authors: [{ name: 'Dev Team', url: 'https://example.com' }],
+  keywords: ['TypeScript', 'App', 'Software'],
+  openGraph: {
+    title: 'FitGirl Repack FuckingFast Downloader',
+    description: 'FitGirl Repack FuckingFast Downloader - Download games from FitGirl Repack with ease.',
+    url: 'https://fg-crawler.vercel.app',
+    siteName: 'FitGirl Repack FuckingFast Downloader',
+    images: [
+      {
+        url: 'https://example.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Open Graph Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'My Application',
+    description: 'A high-performance application built with TypeScript.',
+    creator: '@yourhandle',
+  },
+};
